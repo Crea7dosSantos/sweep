be176f00cab84154ce4eef37fdea868aed0fb89c
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-08-07 21:21:59 +0900

    add DB CRUD processing

--- myapp.py
+++ myapp.py
@@ -1,14 +1,33 @@
-from flask import Flask
+import pymysql
+from flask import Flask, redirect, render_template, request, url_for
 
 # flaskアプリケーションの作成
 app = Flask(__name__)
 
+conn = pymysql.connect(host='localhost', user='flask_user',
+                       password='yusaku0720', db='flask_db', autocommit=True,
+                       cursorclass=pymysql.cursors.DictCursor)
+db = conn.cursor()
 
-# ルーティングを行い、アクションを指定する。
+
 @app.route('/')
 def index():
-    return 'Hello World'
+    db.execute('SELECT * from todos')
+    todos = db.fetchall()
+    return render_template('index.html', todos=todos)
 
 
-# flaskのアプリケーションを実行。引数のdebugにTrueを指定してデバッグモードにすることで、エラー時にブラウザで確認することができる。
+@app.route('/create', methods=('POST',))
+def create():
+    name = request.form['name']
+    db.execute('INSERT INTO todos(name) VALUES (%s)', (name,))
+    return redirect(url_for('index'))
+
+
+@app.route('/<int:id>delete', methods=('POST',))
+def delete(id):
+    db.execute('DELETE FROM todos WHERE id = %s', (id,))
+    return redirect(url_for('index'))
+
+
 app.run(debug=True)
