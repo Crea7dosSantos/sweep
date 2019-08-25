cfd673d4f5405f07891a29c2b57307cf2e8ca0a2
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-08-25 10:29:19 +0900

    database connect from SQLAlchemy

--- myapp.py
+++ myapp.py
@@ -1,20 +1,31 @@
-import pymysql
 from flask import Flask, redirect, render_template, request, url_for
 import setting
 from form import CreateTodoForm, LoginForm, SignUpForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = setting.SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{user}:{password}@{host}/\
+    {db}?charset=utf8'.format(**{
+    'user': setting.USER_NAME,
+    'password': setting.PASSWORD,
+    'host': setting.HOST_NAME,
+    'db': setting.DB_NAME
+})
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = False
+db = SQLAlchemy(app)
 
-conn = pymysql.connect(
-    host=setting.HOST_NAME,
-    user=setting.USER_NAME,
-    password=setting.PASSWORD, db=setting.DB_NAME,
-    autocommit=True,
-    cursorclass=pymysql.cursors.DictCursor
-)
-db = conn.cursor()
+
+# conn = pymysql.connect(
+#     host=setting.HOST_NAME,
+#     user=setting.USER_NAME,
+#     password=setting.PASSWORD, db=setting.DB_NAME,
+#     autocommit=True,
+#     cursorclass=pymysql.cursors.DictCursor
+# )
+# db = conn.cursor()
 
 
 @app.route('/', methods=['GET', 'POST'])
