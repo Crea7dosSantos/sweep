5f524f69e3db9e00413fa6046a6fbfe1c9bf0516
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-08-25 16:36:25 +0900

    connect to mysql with pymysql and sqlalchemy

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, redirect, render_template, request, url_for
 import setting
 from form import CreateTodoForm, LoginForm, SignUpForm
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = setting.SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{user}:{password}@{host}/\
-    {db}?charset=utf8'.format(**{
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{user}:{password}@{host}/{db}?charset=utf8'.format(**{
     'user': setting.USER_NAME,
     'password': setting.PASSWORD,
     'host': setting.HOST_NAME,
@@ -18,14 +18,14 @@ SQLALCHEMY_ECHO = False
 db = SQLAlchemy(app)
 
 
-# conn = pymysql.connect(
-#     host=setting.HOST_NAME,
-#     user=setting.USER_NAME,
-#     password=setting.PASSWORD, db=setting.DB_NAME,
-#     autocommit=True,
-#     cursorclass=pymysql.cursors.DictCursor
-# )
-# db = conn.cursor()
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    title = db.Column(db.String(255), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.datetime.utcnow)
+
+
+db.create_all()
 
 
 @app.route('/', methods=['GET', 'POST'])
