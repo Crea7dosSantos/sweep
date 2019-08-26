34c25c8728ede4e513f1766c78f79c5935108a1e
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-08-26 18:18:57 +0900

    migrate from mysql to sqlite

---
+++ flask_todo/routes.py
@@ -0,0 +1,42 @@
+from flask import render_template, url_for, redirect, request
+from flask_todo import app, db
+from flask_todo.form import CreateTodoForm, LoginForm, SignUpForm
+from flask_todo.models import Todo
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/create', methods=['GET', 'POST'])
+def index():
+    form = CreateTodoForm()
+    if form.validate_on_submit():
+        input_form_data = request.form['todo_name']
+        todo = Todo(title=input_form_data)
+        db.session.add(todo)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    todos = db.session.query(Todo).all()
+    return render_template('index.html', form=form, todos=todos)
+
+
+@app.route('/<int:id>delete', methods=('POST',))
+def delete(id):
+    db.session.query(Todo).filter(Todo.id == id).delete()
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('signup.html', form=form)
---
+++ flask_todo/settings.py
@@ -0,0 +1,15 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+load_dotenv(__file__)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DB_NAME = os.environ.get("DB_NAME")
+USER_NAME = os.environ.get("USER_NAME")
+PASSWORD = os.environ.get("PASSWORD")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+HOST_NAME = os.environ.get("HOST_NAME")
