b89799a255924d3084ec79f2748cd02a5650798a
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-09-01 02:21:00 +0900

    add client  nuxt.js

--- flask_todo/models.py
+++ flask_todo/models.py
@@ -11,5 +11,10 @@ class Todo(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False,
                             default=datetime.datetime.utcnow)
 
-    def __repr__(self):
-        return "Todo('{}', '{}')".format(self.title, self.date_posted)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    # posts = db.relationship('Post', backref='author', lazy=True)
