2bef9016bc3606943cbbc20ef9835310e36daf4a
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-09-04 19:15:07 +0900

    fix cache .git

--- client/plugins/filter.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import moment from 'moment'
-
-Vue.filter('dateFormat', function(dateValue) {
-  if (!dateValue) return ''
-  return moment(dateValue).format('YYYY年MM月DD日 HH時mm分ss秒')
-})
