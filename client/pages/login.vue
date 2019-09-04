8dca6c04d8c59c650e54b7f16da04506e44287ad
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-09-04 22:05:43 +0900

    add basic components

---
+++ client/pages/login.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="hero is-primary is-fullheight-with-navbar">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-one-third" />
+          <div class="column card box">
+            <div class="field form-title">
+              <h3 class="title is-2 has-text-dark">login</h3>
+            </div>
+            <div class="field">
+              <input class="input is-medium" type="email" placeholder="Email" />
+            </div>
+            <div class="field">
+              <input type="password" class="input is-medium" placeholder="Password" />
+            </div>
+            <div class="columns">
+              <div class="column is-half"></div>
+              <div class="column form-subject">
+                <div class="field">
+                  <button class="button is-light">Cancel</button>
+                  <button class="button is-dark">Login</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style scoped>
+.card {
+  background: white;
+  height: 300px;
+}
+.form-title {
+  text-align: center;
+  margin-bottom: 30px;
+}
+.form-subject {
+  margin-top: 15px;
+}
+.input {
+  margin-top: 10px;
+}
+</style>
