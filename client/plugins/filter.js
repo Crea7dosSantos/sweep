c30ca6e91395cca46f360b3d0adea2b905b592ec
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-09-07 18:10:03 +0900

    change diplay date

--- client/plugins/filter.js
+++ client/plugins/filter.js
@@ -3,5 +3,5 @@ import moment from 'moment'
 
 Vue.filter('dateFormat', function(dateValue) {
   if (!dateValue) return ''
-  return moment(dateValue).format('YYYY年MM月DD日 HH時mm分ss秒')
+  return moment(dateValue).format('YYYY年MM月DD日')
 })
