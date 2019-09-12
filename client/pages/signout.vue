a9925277dfe10facbe806895c3de6abda7aa0405
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-09-13 04:48:14 +0900

    add the ability to delete stored cookies when signing out

--- client/pages/signout.vue
+++ client/pages/signout.vue
@@ -1,21 +1,18 @@
 <template>
   <div>
-    <button
-      class="button is-praimary"
-      @click="signout"
-    >
-      Sign out
-    </button>
+    <button class="button is-praimary" @click="signout">Sign out</button>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import Cookies from 'js-cookie'
 
 export default {
   methods: {
     ...mapActions('user', ['signOut']),
     signout: function() {
+      Cookies.remove('jwt_token')
       this.signOut()
     }
   }
