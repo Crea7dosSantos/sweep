bb4b73588b7327d5842cd63dcb3a5c2236a75ce2
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-09-19 08:25:18 +0900

    complete migration modal window

---
+++ client/components/Signin.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-dialog v-model="userSigninVisible" width="400px">
+    <v-card class="elevation-12">
+      <v-toolbar flat>
+        <v-toolbar-title class="grey--text">signIn form</v-toolbar-title>
+        <div class="flex-grow-1" />
+      </v-toolbar>
+      <v-divider />
+      <v-card-text>
+        <v-form ref="form">
+          <v-text-field
+            v-model="email"
+            label="Email"
+            :rules="[rules.required, rules.email]"
+            name="Email"
+            prepend-icon="email"
+            type="email"
+            required
+          />
+          <v-text-field
+            id="password"
+            v-model="password"
+            :rules="[rules.required, rules.min4, rules.max20]"
+            label="Password"
+            name="password"
+            prepend-icon="lock"
+            type="password"
+            required
+          />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <div class="flex-grow-1" />
+        <v-btn color="primary" @click="signin">Sign in</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import axiosBase from '@/plugins/axiosBase'
+import Cookies from 'js-cookie'
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  props: {},
+  data: () => ({
+    drawer: null,
+    email: '',
+    password: '',
+    rules: {
+      required: value => !!value || 'Required.',
+      min4: value => value.length >= 4 || 'Min 4 characters',
+      max20: value => value.length <= 20 || 'Max 20 characters',
+      email: value => {
+        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return pattern.test(value) || 'Invalid e-mail.'
+      }
+    }
+  }),
+  computed: {
+    ...mapState('modal', ['isSigninView']),
+    userSigninVisible: {
+      get() {
+        return this.isSigninView
+      },
+      set() {
+        return this.$store.dispatch('modal/unsetSigninView')
+      }
+    }
+  },
+  methods: {
+    ...mapActions('user', ['signIn']),
+    ...mapActions('snackbar', ['snackOn']),
+    ...mapActions('modal', ['unsetSigninView']),
+    signin: function() {
+      const router = this.$router
+
+      if (!this.$refs.form.validate()) {
+        return
+      }
+      let self = this
+      axiosBase
+        .post('/signin', {
+          email: self.email,
+          password: self.password
+        })
+        .then(res => {
+          this.snackOn({ payload: 'Sign in Success!', color: 'green' })
+          this.signIn(res.data.access_token)
+          Cookies.set('access_token', res.data.access_token)
+          Cookies.set('refresh_token', res.data.refresh_token)
+          this.unsetSigninView()
+          router.push('/home')
+        })
+        .catch(() => {
+          this.snackOn({ payload: 'An error has occured', color: 'error' })
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-divider {
+  margin: 0px;
+}
+</style>
