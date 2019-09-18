390d86b1067d99ca0a8cc1164546dce1fcaefc3c
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-09-19 08:39:50 +0900

    create new components file

---
+++ client/components/AllModal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <User />
+    <Signin />
+    <signup />
+    <Signout />
+  </div>
+</template>
+
+<script>
+import User from '~/components/User'
+import Signin from '~/components/Signin'
+import Signup from '~/components/Signup'
+import Signout from '~/components/Signout'
+
+export default {
+  components: {
+    User,
+    Signin,
+    Signup,
+    Signout
+  }
+}
+</script>
