0b818fe50838280f09c2173160699b526e2fc559
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-09-19 07:03:37 +0900

    add tmp method

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -2,15 +2,11 @@
   <div>
     <v-app class="inspire">
       <div>
-        <v-navigation-drawer
-          v-model="drawer"
-          absolute
-          temporary
-        >
+        <v-navigation-drawer v-model="drawer" absolute temporary>
           <v-list :class="{ 'is-hidden': !isAuthenticated }">
             <v-list-item>
               <v-list-item-avatar>
-                <img src="~/assets/cristiano.jpg">
+                <img src="~/assets/cristiano.jpg" />
               </v-list-item-avatar>
               <v-list-item-title>{{ user.name }}</v-list-item-title>
             </v-list-item>
@@ -26,16 +22,9 @@
               hide-details
             />
           </v-list>
-          <v-list
-            class="pt-0"
-            dense
-          >
+          <v-list class="pt-0" dense>
             <v-divider />
-            <v-list-item-group
-              v-for="item in items1"
-              :key="item.title"
-              link
-            >
+            <v-list-item-group v-for="item in items1" :key="item.title" link>
               <v-list-item :to="item.title">
                 <v-list-item-icon>
                   <v-icon>{{ item.icon }}</v-icon>
@@ -78,33 +67,13 @@
             </v-list-item-group>
           </v-list>
         </v-navigation-drawer>
-        <v-toolbar
-          dark
-          color="grey darken-3"
-        >
-          <v-app-bar-nav-icon
-            class="hidden-md-and-up"
-            @click.stop="drawer = !drawer"
-          />
+        <v-toolbar dark color="grey darken-3">
+          <v-app-bar-nav-icon class="hidden-md-and-up" @click.stop="drawer = !drawer" />
           <v-toolbar-items>
-            <v-btn
-              text
-              to="/"
-            >
-              Top
-            </v-btn>
+            <v-btn text to="/">Top</v-btn>
           </v-toolbar-items>
-          <v-toolbar-items
-            v-for="item in items1"
-            :key="item.title"
-            class="hidden-sm-and-down"
-          >
-            <v-btn
-              text
-              :to="item.title"
-            >
-              {{ item.title }}
-            </v-btn>
+          <v-toolbar-items v-for="item in items1" :key="item.title" class="hidden-sm-and-down">
+            <v-btn text :to="item.title">{{ item.title }}</v-btn>
           </v-toolbar-items>
           <v-spacer />
           <v-expand-x-transition>
@@ -120,66 +89,37 @@
             />
           </v-expand-x-transition>
           <v-toolbar-items class="hidden-sm-and-down">
-            <v-btn
-              icon
-              @click="showSearchInput = !showSearchInput"
-            >
+            <v-btn icon @click="showSearchInput = !showSearchInput">
               <v-icon>search</v-icon>
             </v-btn>
           </v-toolbar-items>
-          <v-toolbar-items
-            v-for="item in items3"
-            :key="item.title"
-            class="hidden-sm-and-down"
-          >
+          <v-toolbar-items v-for="item in items3" :key="item.title" class="hidden-sm-and-down">
             <v-btn
               text
               :class="{ 'is-hidden': isAuthenticated }"
-              :to="item.title"
-            >
-              {{ item.title }}
-            </v-btn>
+              @click="tmp(item.title)"
+            >{{ item.title }}</v-btn>
           </v-toolbar-items>
           <v-toolbar-items class="hidden-sm-and-down">
             <v-btn
               text
               :class="{ 'is-hidden': !isAuthenticated }"
               @click="displayUserView"
-            >
-              {{ user.name }}
-            </v-btn>
+            >{{ user.name }}</v-btn>
           </v-toolbar-items>
-          <v-toolbar-items
-            v-for="item in items2"
-            :key="item.title"
-            class="hidden-sm-and-down"
-          >
+          <v-toolbar-items v-for="item in items2" :key="item.title" class="hidden-sm-and-down">
             <v-btn
               text
               :class="{ 'is-hidden': !isAuthenticated }"
-              :to="item.title"
-            >
-              {{ item.title }}
-            </v-btn>
+              @click="tmp(item.title)"
+            >{{ item.title }}</v-btn>
           </v-toolbar-items>
-          {{ isUserView }}
         </v-toolbar>
       </div>
       <div>
-        <v-snackbar
-          v-model="snackbarVisible"
-          :color="actionStatus"
-          top
-          :timeout="timeout"
-        >
+        <v-snackbar v-model="snackbarVisible" :color="actionStatus" top :timeout="timeout">
           {{ message }}
-          <v-btn
-            color="white"
-            text
-            @click="close"
-          >
-            Close
-          </v-btn>
+          <v-btn color="white" text @click="close">Close</v-btn>
         </v-snackbar>
       </div>
       <nuxt />
@@ -214,10 +154,12 @@ export default {
       { title: 'home', icon: 'person' },
       { title: 'documentation', icon: 'dashboard' }
     ],
-    items2: [{ title: 'signout', icon: 'dashboard' }],
+    items2: [
+      { title: 'signout', icon: 'dashboard', method: 'displaySignoutView' }
+    ],
     items3: [
-      { title: 'signup', icon: 'account_box' },
-      { title: 'signin', icon: 'gavel' }
+      { title: 'signup', icon: 'account_box', method: 'displaySignupView' },
+      { title: 'signin', icon: 'gavel', method: 'displaySigninView' }
     ]
   }),
   methods: {
@@ -227,8 +169,25 @@ export default {
       this.snackOff()
     },
     displayUserView: function() {
-      console.log('dispalay')
       this.setUserView()
+    },
+    displaySignupVie: function() {
+      console.log('signup')
+    },
+    displaySigninView: function() {
+      console.log('signin')
+    },
+    displaySignoutView: function() {
+      console.log('signout')
+    },
+    tmp: function(data) {
+      if (data === 'signout') {
+        this.displaySignoutView()
+      } else if (data === 'signin') {
+        this.displaySigninView()
+      } else if (data === 'signup') {
+        this.displaySignupVie()
+      }
     }
   }
 }
