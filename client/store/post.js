5602fda39d5389bf50900a3dc8cf148c188a9137
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-09-28 15:57:24 +0900

    create post data file in store directory

---
+++ client/store/post.js
@@ -0,0 +1,34 @@
+import Axios from 'axios'
+export const strict = false
+
+export const state = () => ({
+  posts: []
+})
+
+export const mutations = {
+  init(state, payload) {
+    state.posts = payload
+  }
+}
+
+export const actions = {
+  init({ commit }, token) {
+    const axiosAccess = Axios.create({
+      baseURL: 'http://localhost:5000',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token
+      },
+      responseType: 'json'
+    })
+    axiosAccess
+      .get('/home')
+      .then(res => {
+        const data = res.data.todos
+        commit('init', data)
+      })
+      .catch(() => {
+        console.log('stateのhome部分でエラってる。')
+      })
+  }
+}
